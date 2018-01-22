62dd5d8c99e9dfa279bcb6db90fafda985f6d803
xiaomiwujiecao <1410200695@qq.com>
2018-01-23 01:19:19 +0800

    删除lodash

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 //压缩,合并 js
+//重命名
+const rename = require("gulp-rename");
 gulp.task('minifyjs', function() {
-	return gulp.src('build/**/*.js')      //需要操作的文件
-		.pipe(concat('jsregexp.js'))    //合并所有js到bundle.js
+	return gulp.src('browser/**/*.js')      //需要操作的文件//合并所有js到bundle.js
 		.pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
 		.pipe(uglify())    //压缩
 		.pipe(gulp.dest('build/dist'))
